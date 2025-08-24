9720f8493672f74d92b0430dc07860ad64f7989d
Ann <Annubaka223@gmail.com>
2025-08-23 23:25:58 -0400

    init commit

---
+++ DrinkWater/DrinkWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkWaterApp.swift
+//  DrinkWater
+//
+//  Created by Ann Ubaka on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct DrinkWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrinkWaterTests/DrinkWaterTests.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkWaterTests.swift
+//  DrinkWaterTests
+//
+//  Created by Ann Ubaka on 8/23/25.
+//
+
+import Testing
+@testable import DrinkWater
+
+struct DrinkWaterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
