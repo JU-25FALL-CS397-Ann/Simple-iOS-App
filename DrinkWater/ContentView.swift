30cac6f89aacd8c3f22835961bb28a0c842804a8
Ann <Annubaka223@gmail.com>
2025-08-24 00:11:46 -0400

    added main logic

--- DrinkWater/ContentView.swift
+++ DrinkWater/ContentView.swift
@@ -8,15 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var buttonText = "Parched."
+    @State private var alt = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "heart")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                
+            Button(action:
+                    {
+                if alt == false {
+                    self.buttonText = "Hydrated."
+                    alt = true
+                } else {
+                    self.buttonText = "Parched."
+                    alt = false
+                }
+            }, label: {
+                if alt == false {
+                    Text(buttonText)
+                    .font(.title)
+                    .padding()
+                    .background(Color.orange)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                } else {
+                    Text(buttonText)
+                    .font(.title)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                }
+            })
+            .padding()
+            Text("Welcome to the simple 'Drink Water' app where you can tap a button to become hydrated. :)")
+                .foregroundColor(.white)
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity) // Expand to fill screen
+        .background(Color(red: 0.2, green: 0.3, blue: 0.4).ignoresSafeArea())
+
     }
+
 }
 
 #Preview {
